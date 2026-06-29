06adc7eb54b1d032e447405655ed409cf967431f
snikmas <mary.snikk@gmail.com>
2026-06-29 20:21:18 +0800

    pref: fix bugs

--- src/db/models.py
+++ src/db/models.py
@@ -8,32 +8,32 @@ class Base(DeclarativeBase):
     pass
 
 class Ticket(Base):
-    __table__ = 'tickets'
-    id: Mapped[str] = mapped_column(int, primary_key=True)
+    __tablename__ = 'tickets'
+    id: Mapped[str] = mapped_column(str, primary_key=True)
     title: Mapped[str] = mapped_column(String(255))
     description: Mapped[str] = mapped_column(String(32000))
     category: Mapped[Category] = mapped_column(Enum(Category))
-    tags: Mapped[List[Tag]] = mapped_column(Tag, nullable=True)
+    tags: Mapped[List[str]] = mapped_column(str, nullable=True)
     assigned_agent_id: Mapped[Optional[str]] = mapped_column(
         ForeignKey('users.id', ondelete='NOTHING'),
         String(36), 
         nullable=True)
-    creasor_user_id: Mapped[str] = mapped_column(
+    creator_user_id: Mapped[str] = mapped_column(
         ForeignKey('user.id', ondelete='NOTHING'), 
         String(36))
-    status: Mapped[Status] = mapped_column(Status)
-    priority: Mapped[Priority] = mapped_column(Priority)
+    status: Mapped[Status] = mapped_column(Enum(Status))
+    priority: Mapped[Priority] = mapped_column(Enum(Priority))
 
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     # due_at: Mapped[Optonal[timedelta]] = mapped_column(Interval)
 
-    def __rept__(self) -> str:
+    def __repr__(self) -> str:
         pass
 
 
 class User(Base):
-    __table__ = 'users'
+    __tablename__ = 'users'
     id: Mapped[str] = mapped_column(primary_key=True)
     nickname: Mapped[str] = mapped_column(String(128), unique=True)
     avatar_url: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
@@ -41,8 +41,8 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(String(50))
     phone: Mapped[str] = mapped_column(String(24), unique=True)
     email: Mapped[str] = mapped_column(String(254), unique=True)
-    role: Mapped[Role] = mapped_column(Role)
+    role: Mapped[Role] = mapped_column(Enum(Role))
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    def __rept__(self) -> str:
+    def __repr__(self) -> str:
         pass
